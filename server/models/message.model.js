7956e019001868c118226c07c83626ebcad23973
Garam Yoon <rkfka9536@gmail.com>
2026-01-25 18:25:56 -0500

    Fix sender field to use ObjectId FK to User model

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
   section: String,
-  sender: String,
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   message: String,
   timestamp: { type: Date, default: Date.now }
 });
